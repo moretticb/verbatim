26beb16f85d2325b9bb480de0f99d2f5a1f41d69
Caio Benatti Moretti <caiodba@gmail.com>
2023-03-17 15:19:43 +0100

    organizing in folders

--- tools/text_chatgpt.sh
+++ tools/text_chatgpt.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-
 echo "Ask: $1"
 
-line="$(echo $1 | ./text_chatgpt_tool.sh | fold -w 60 -s | choose -n 10 -s 24)"
+line="$(echo $1 | src/text_chatgpt_tool.sh | fold -w 60 -s | choose -n 10 -s 24)"
 
 if [ $? = 1 ]; then
         exit 1;
